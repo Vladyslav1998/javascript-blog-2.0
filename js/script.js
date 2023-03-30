489b6e92c2fd54e516c0b108e0e91d7c2d8cbdfb
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2023-03-30 15:57:43 +0200

    Add fix bag in activeLinks

--- js/script.js
+++ js/script.js
@@ -10,11 +10,12 @@ const titleClickHandler = function (event) {
 
    /* remove class 'active' from all article links  */
 
-   const activeLinks = document.querySelectorAll('.titles a .active');
+   const activeLinks = document.querySelectorAll('.titles a.active');
 
-   for( let activeLink of activeLinks){
+   for (let activeLink of activeLinks) {
       activeLink.classList.remove('active');
    }
+
 
    /* add class 'active' to the clicked link */
 
