62b958249b9736462aff7de7e5d0b05262b73215
Vladyslav Mazurenko <vladmazurenko13798@gmail.com>
2023-04-12 16:12:41 +0200

    Delete old function titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 {
    'use strict';
-   // document.getElementById('test-button').addEventListener('click', function(){
-   //    const links = document.querySelectorAll('.titles a');
-   //    console.log('Links :', links);
-   // });
 
    const titleClickHandler = function (event) {
       event.preventDefault();
